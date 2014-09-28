e9dc979bc8b6d638014cb3e39bc92e0a145e2a9e
Giovanni Collazo <hello@gcollazo.com>
2014-09-27 21:44:13 -0400

    Cache packages on localStorage

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,7 +2,22 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
+    var cache = window.localStorage;
+
+    if ('emberCLIAddons' in cache && 'lastUpdate' in cache) {
+      var lastUpdate = (new Date() - new Date(cache.lastUpdate)) / 1000,
+          intervalInSeconds = 5 * 60;
+
+      if (lastUpdate < intervalInSeconds) {
+        return JSON.parse(cache.emberCLIAddons);
+      }
+    }
+
     var url = 'http://ember-addons-server.herokuapp.com/';
-    return Ember.$.getJSON(url);
+    return Ember.$.getJSON(url).then(function(data) {
+      cache.emberCLIAddons = JSON.stringify(data);
+      cache.lastUpdate = new Date();
+      return data;
+    });
   }
 });
