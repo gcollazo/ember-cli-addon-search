2c2e76ca301a5831c29e87d2721ee53d9e25e584
Giovanni Collazo <hello@gcollazo.com>
2014-09-29 21:46:46 -0400

    Adds Travis badges

--- app/controllers/package.js
+++ app/controllers/package.js
@@ -7,5 +7,11 @@ export default Ember.ObjectController.extend({
 
   gravatarURL: function() {
     return this.get('doc._npmUser.gravatar') + '?s=30&d=retro';
-  }.property('doc._npmUser.gravatar')
+  }.property('doc._npmUser.gravatar'),
+
+  travisBadgeURL: function() {
+    var user = this.get('doc.github.user'),
+        repo = this.get('doc.github.repo');
+    return 'https://travis-ci.org/'+ user +'/'+ repo +'.svg?branch=master';
+  }.property()
 });
