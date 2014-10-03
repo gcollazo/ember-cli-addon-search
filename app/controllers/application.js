bc293cdf376dc72bae664a480ac15266f971db47
Giovanni Collazo <hello@gcollazo.com>
2014-10-03 14:21:53 -0400

    Cleanup

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,18 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
-  queryParams:['query'],
+  queryParams: ['query'],
 
   packageCount: function() {
     return this.get('content').length;
   }.property('content'),
 
   filteredContent: function() {
-    var self = this;
+    var searchTerm = this.get('query');
 
-    if (this.get('query')) {
+    if (searchTerm) {
       return this.get('content').filter(function(item) {
-        var query = self.get('query').toLowerCase(),
+        var query = searchTerm.toLowerCase(),
             name = (item.name || '').toLowerCase(),
             desc = (item.doc.description || '').toLowerCase(),
             author = (item.doc._npmUser.name || '').toLowerCase();
