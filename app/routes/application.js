8c8a3ef865614d2faf92896d94615977870a2280
Giovanni Collazo <hello@gcollazo.com>
2014-10-06 19:26:33 -0400

    Stop caching data

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,22 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    var cache = window.localStorage;
-
-    if (cache && 'emberCLIAddons' in cache && 'lastUpdate' in cache) {
-      var lastUpdate = (new Date() - new Date(cache.lastUpdate)) / 1000,
-          intervalInSeconds = 5 * 60;
-
-      if (lastUpdate < intervalInSeconds) {
-        return JSON.parse(cache.emberCLIAddons);
-      }
-    }
-
     var url = 'https://io-builtwithember-addons-data.s3.amazonaws.com/addons.json';
-    return Ember.$.getJSON(url).then(function(data) {
-      cache.emberCLIAddons = JSON.stringify(data);
-      cache.lastUpdate = new Date();
-      return data;
-    });
+    return Ember.$.getJSON(url);
   }
 });
