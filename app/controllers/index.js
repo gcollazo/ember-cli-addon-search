68f5c217fb9eabf235a63a7ed986a0cf90753fb9
Stefan Penner <stefan.penner@gmail.com>
2014-10-06 22:05:02 -0400

    improve filtering costs dramatically

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,21 +3,37 @@ import Ember from 'ember';
 export default Ember.ArrayController.extend({
   queryParams: ['query'],
 
+  init: function() {
+    this._filteredArray = [];
+    this._super();
+  },
   packageCount: Ember.computed.readOnly('content.length'),
 
   filteredContent: function() {
     var controller = this;
 
-    if (this.get('query')) {
-      return this.get('content').filter(function(item) {
-        var query = controller.get('query').toLowerCase(),
-            name = (item.name || '').toLowerCase(),
-            desc = (item.description || '').toLowerCase(),
-            author = (item._npmUser.name || '').toLowerCase();
+    var count = 0;
+    var filtered = this._filteredArray;
+    var content = this.get('content');
 
-        return name.match(query) || desc.match(query) || author.match(query);
-      });
-    }
-    return this.get('content');
+    content.forEach(function(item) {
+      var query = controller.get('query');
+      var name = (item.name || '').toLowerCase();
+      var desc = (item.description || '').toLowerCase();
+      var author = (item._npmUser.name || '').toLowerCase();
+
+      if (query) {
+        query = query.toLowerCase();
+      }
+
+      if (!query || name.match(query) || desc.match(query) || author.match(query)) {
+        filtered.addObject(item);
+      } else {
+        filtered.removeObject(item);
+      }
+      count++;
+    });
+
+    return filtered;
   }.property('query').readOnly()
 });
