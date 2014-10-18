28eb44bb23db9313f28d2eaac440694dec3cfea7
Giovanni Collazo <hello@gcollazo.com>
2014-10-18 13:30:58 -0400

    Debounce approach

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -9,15 +9,24 @@ export default Ember.ArrayController.extend({
   },
   packageCount: Ember.computed.readOnly('content.length'),
 
+  updateQueryParam: function() {
+    this.set('queryProxy', this.get('query'));
+  }.observes('query'),
+
   filteredContent: function() {
     var controller = this;
+
+    // Update the query param
+    Ember.run.debounce(this, function(){
+      this.set('query', this.get('queryProxy'));
+    }, 1000);
 
     var count = 0;
     var filtered = this._filteredArray;
     var content = this.get('content');
 
     content.forEach(function(item) {
-      var query = controller.get('query');
+      var query = controller.get('queryProxy');
       var name = (item.name || '').toLowerCase();
       var desc = (item.description || '').toLowerCase();
       var author = (item._npmUser.name || '').toLowerCase();
@@ -35,5 +44,5 @@ export default Ember.ArrayController.extend({
     });
 
     return filtered;
-  }.property('query').readOnly()
+  }.property('queryProxy').readOnly()
 });
