389c80095abb52f454dec04a58deda3dae4516ff
Giovanni Collazo <hello@gcollazo.com>
2015-01-29 19:30:32 -0400

    Fix bootstrap requirement

--- Brocfile.js
+++ Brocfile.js
@@ -17,6 +17,6 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
-app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+app.import('bower_components/bootstrap/dist/css/bootstrap.min.css');
 
 module.exports = app.toTree();
