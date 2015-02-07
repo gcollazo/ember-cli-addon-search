24f0813c9ce778a2ee35a8d929f24785673eee7e
Giovanni Collazo <hello@gcollazo.com>
2015-02-06 23:13:07 -0400

    Adds travis badge url

--- app/components/em-travis-badge.js
+++ app/components/em-travis-badge.js
@@ -2,11 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   travisBadgeURL: function() {
+    return this.get('travisLinkURL') + '.svg?branch=master';
+  }.property('github').readOnly(),
+
+  travisLinkURL: function() {
     var _github = this.get('github');
-
     if (_github) {
-      return 'https://travis-ci.org/' + _github.user + '/' + _github.repo + '.svg?branch=master';
+      return 'https://travis-ci.org/' + _github.user + '/' + _github.repo;
     }
-
   }.property('github').readOnly()
 });
