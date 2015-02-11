a44ec99b15ac641819cb8c2efd37e9cb3c594dd7
Giovanni Collazo <hello@gcollazo.com>
2015-02-11 14:28:44 -0400

    Implements basic pagination

--- app/components/em-pkg.js
+++ app/components/em-pkg.js
@@ -3,7 +3,6 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'tr',
   classNames: ['package'],
-  classNameBindings: ['matchFilter:visible:hidden'],
 
   owner: Ember.computed.readOnly('pkg._npmUser.name'),
   name: Ember.computed.readOnly('pkg.name'),
@@ -15,13 +14,5 @@ export default Ember.Component.extend({
 
   npmPackageURL: function() {
     return 'https://npmjs.org/' + this.get('pkg.name');
-  }.property('pkg.name').readOnly(),
-
-  matchFilter: function() {
-    var query = this.get('query');
-    return !query ||
-      this.get('name').toLowerCase().indexOf(query.toLowerCase()) >= 0 ||
-      this.get('owner').toLowerCase().indexOf(query.toLowerCase()) >= 0 ||
-      this.get('description').toLowerCase().indexOf(query.toLowerCase()) >= 0;
-  }.property('query').readOnly()
+  }.property('pkg.name').readOnly()
 });
--- app/controllers/packages/list.js
+++ app/controllers/packages/list.js
@@ -3,11 +3,61 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   queryParams: ['query'],
 
+  offset: 0,
+  limit: 20,
+
+  nothingFound: Ember.computed.equal('foundCount', 0),
+  nextDisabled: Ember.computed.not('hasNextPage'),
+  previousDisabled: Ember.computed.not('hasPreviousPage'),
+
+  onQueryChange: function() {
+    this.set('offset', 0);
+  }.observes('query'),
+
+  currentPageContent: function() {
+    var offset = this.get('offset'),
+        limit = this.get('limit'),
+        query = this.get('query');
+
+    var result = this.get('model').filter(function(item) {
+      return !query ||
+        item.name.toLowerCase().indexOf(query.toLowerCase()) >= 0 ||
+        item._npmUser.name.toLowerCase().indexOf(query.toLowerCase()) >= 0 ||
+        item.description.toLowerCase().indexOf(query.toLowerCase()) >= 0;
+    });
+
+    this.set('foundCount', result.length);
+
+    return result.slice(offset, offset + limit);
+  }.property('offset', 'model', 'query').readOnly(),
+
+  hasPreviousPage: function() {
+    return this.get('offset') !== 0;
+  }.property('offset'),
+
+  hasNextPage: function() {
+    var offset = this.get('offset'),
+        limit = this.get('limit'),
+        length = this.get('foundCount');
+
+    return (offset + limit) < length;
+  }.property('offset', 'limit', 'foundCount'),
+
   actions: {
     sortBy: function(by, reverse) {
       var sorted =  this.get('model').sortBy(by);
       if (reverse) { sorted.reverse(); }
       this.set('model', sorted);
+    },
+
+    nextPage: function() {
+      var limit = this.get('limit');
+      this.incrementProperty('offset', limit);
+    },
+
+    previousPage: function() {
+      var limit = this.get('limit');
+      this.decrementProperty('offset', limit);
     }
   }
 });
