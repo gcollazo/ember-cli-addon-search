d4a43899add1018bfed2d6a112f18ed1efccd420
Giovanni Collazo <hello@gcollazo.com>
2015-02-11 15:17:43 -0400

    Remove unused computed.alias

--- app/components/em-pkg.js
+++ app/components/em-pkg.js
@@ -4,13 +4,11 @@ export default Ember.Component.extend({
   tagName: 'tr',
   classNames: ['package'],
 
-  owner: Ember.computed.readOnly('pkg._npmUser.name'),
   name: Ember.computed.readOnly('pkg.name'),
   description: Ember.computed.readOnly('pkg.description'),
   downloads: Ember.computed.readOnly('pkg.downloads.downloads'),
   updated: Ember.computed.readOnly('pkg.time.modified'),
   created: Ember.computed.readOnly('pkg.time.created'),
-  github: Ember.computed.readOnly('pkg.github'),
 
   npmPackageURL: function() {
     return 'https://npmjs.org/' + this.get('pkg.name');
