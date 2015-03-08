6ea24e95c2f792bd7c5c0a2c852ef00536575eb8
Ben <benjamin.schoenburg@madvertise.com>
2015-03-08 11:27:57 +0100

    dont add query change to browser history

--- app/routes/packages/list.js
+++ app/routes/packages/list.js
@@ -2,6 +2,12 @@ import Ember from 'ember';
 import ajax from 'ic-ajax';
 
 export default Ember.Route.extend({
+  queryParams: {
+    query: {
+      replace: true
+    }
+  },
+
   model: function() {
     var url = 'https://io-builtwithember-addons-data.s3.amazonaws.com/addons.json';
     return ajax(url).then(function(result) {
