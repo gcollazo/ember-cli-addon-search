40966de79874668594089915bd97fb99df47d0ff
Ben <benjamin.schoenburg@madvertise.com>
2015-03-09 17:31:02 +0100

    reduce computed properties

--- app/components/em-pkg.js
+++ app/components/em-pkg.js
@@ -6,8 +6,6 @@ export default Ember.Component.extend({
   tagName: 'tr',
   classNames: ['package'],
 
-  name: Ember.computed.readOnly('pkg.name'),
-  description: Ember.computed.readOnly('pkg.description'),
   downloads: Ember.computed.readOnly('pkg.downloads.downloads'),
   updated: Ember.computed.readOnly('pkg.time.modified'),
 
@@ -17,8 +15,8 @@ export default Ember.Component.extend({
   }.property('user.gravatar').readOnly(),
 
   npmPackageURL: function() {
-    return npmBaseURL + this.get('name');
-  }.property('name').readOnly(),
+    return npmBaseURL + this.get('pkg.name');
+  }.property('pkg.name').readOnly(),
 
   npmAuthorURL: function() {
     return npmBaseURL + '~' + this.get('user.name');
