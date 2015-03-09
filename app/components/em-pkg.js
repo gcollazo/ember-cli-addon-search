22666aa97be68c6b7255b649f61880e89660be61
Ben <benjamin.schoenburg@madvertise.com>
2015-03-09 20:59:42 +0100

    htmlbars is so awesome

--- app/components/em-pkg.js
+++ app/components/em-pkg.js
@@ -1,26 +1,11 @@
 import Ember from 'ember';
 
-var npmBaseURL = 'https://npmjs.org/';
-
 export default Ember.Component.extend({
   tagName: 'tr',
   classNames: ['package'],
 
   downloads: Ember.computed.readOnly('pkg.downloads.downloads'),
-  updated: Ember.computed.readOnly('pkg.time.modified'),
-
   user: Ember.computed.readOnly('pkg._npmUser'),
-  gravatar: function() {
-    return this.get('user.gravatar') + '?s=30&d=retro';
-  }.property('user.gravatar').readOnly(),
-
-  npmPackageURL: function() {
-    return npmBaseURL + this.get('pkg.name');
-  }.property('pkg.name').readOnly(),
-
-  npmAuthorURL: function() {
-    return npmBaseURL + '~' + this.get('user.name');
-  }.property('user.name').readOnly(),
 
   isNew: function () {
     return moment().diff(this.get('pkg.time.created'), 'days') <= 7;
