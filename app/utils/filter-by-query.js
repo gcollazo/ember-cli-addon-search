30025c903557fe88591750321bbf3fdd03c53f68
José Padilla <jpadilla@webapplicate.com>
2015-06-13 08:55:06 -0400

    ES6 const/let tweak

--- app/utils/filter-by-query.js
+++ app/utils/filter-by-query.js
@@ -2,11 +2,11 @@ import Ember from 'ember';
 import filterByQuery from 'ember-cli-filter-by-query/util/filter';
 
 export default function computedFilterByQuery(dependentKey, propertyKeys, queryKey, options) {
-  let propertyKeysArray = Ember.makeArray(propertyKeys), array, query;
+  const propertyKeysArray = Ember.makeArray(propertyKeys);
 
   return Ember.computed(queryKey, `${ dependentKey }.@each.{${ propertyKeysArray.join(',') }}`, function() {
-    query = this.get(queryKey) || '';
-    array = this.get(dependentKey);
+    let query = this.get(queryKey) || '';
+    let array = this.get(dependentKey);
 
     return filterByQuery(array, propertyKeys, query, options);
 
