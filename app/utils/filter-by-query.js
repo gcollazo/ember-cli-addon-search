c8a4c94af36e908c07ff4067f1bbc501bee3a3ef
José Padilla <jpadilla@webapplicate.com>
2015-06-15 07:16:32 -0400

    Fix search filtering

--- app/utils/filter-by-query.js
+++ app/utils/filter-by-query.js
@@ -1,5 +1,44 @@
+/*global Sifter*/
 import Ember from 'ember';
-import filterByQuery from 'ember-cli-filter-by-query/util/filter';
+
+// Based on https://github.com/lazybensch/ember-cli-filter-by-query
+// Temporary fix until a fix lands for issue #5
+const filterByQuery = function(array, propertyKeys, query, opt) {
+  let options = opt,
+    input, sifter, result, hash;
+
+  if (!query) {
+    return array;
+  }
+
+  if (Ember.typeOf(options) === 'undefined') {
+    options = {};
+  }
+
+  input = array.map(function(item) {
+    hash = {};
+
+    propertyKeys.forEach(function(key) {
+      hash[key] = Ember.get(item, key);
+    });
+
+    return hash;
+  });
+
+  options.fields = options.fields || propertyKeys;
+  options.limit = options.limit || array.length;
+  options.sort = propertyKeys.map(function(key) {
+    return {field: key, direction: 'asc'};
+  });
+
+  sifter = new Sifter(input);
+  result = sifter.search(query, options);
+
+  return result.items.map(function(item) {
+    return array.objectAt(item.id);
+  });
+
+};
 
 export default function computedFilterByQuery(dependentKey, propertyKeys, queryKey, options) {
   const propertyKeysArray = Ember.makeArray(propertyKeys);
