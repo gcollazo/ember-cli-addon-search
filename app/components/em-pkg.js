e8f2b813c26eaf2f14fdd5c540f4eb9a27e2ff97
Emmanuel Luciano <emma.luciano11@gmail.com>
2015-06-25 13:15:41 -0400

    Implements install command clipboard.

--- app/components/em-pkg.js
+++ app/components/em-pkg.js
@@ -7,6 +7,17 @@ export default Ember.Component.extend({
 
   downloads: Ember.computed.readOnly('pkg.downloads.downloads'),
   user: Ember.computed.readOnly('pkg._npmUser'),
+  hideClipboard: true,
+  hideInstallCommand: true,
+
+  didInsertElement: function() {
+    this.$().hover(() => {
+      this.set('hideClipboard', false);
+    }, () => {
+      this.set('hideClipboard', true);
+      this.set('hideInstallCommand', true);
+    });
+  },
 
   isNew: Ember.computed('pkg.time.created', function() {
     return moment().diff(this.get('pkg.time.created'), 'days') <= 7;
@@ -14,6 +25,11 @@ export default Ember.Component.extend({
 
   hasDescription: Ember.computed('pkg.description', function() {
     return this.get('pkg.description') !== 'The default blueprint for ember-cli addons.';
-  }).readOnly()
+  }).readOnly(),
 
+  actions: {
+    displayInstallCommand() {
+      this.set('hideInstallCommand', false);
+    }
+  }
 });
