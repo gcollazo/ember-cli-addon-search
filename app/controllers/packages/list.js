1f92d8b241ccfbfb9d1dfcfe983095eeb597a7bb
Giovanni Collazo <hello@gcollazo.com>
2015-06-26 10:45:21 -0400

    Slim down the header

--- app/controllers/packages/list.js
+++ app/controllers/packages/list.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 import computedFilterByQuery from '../../utils/filter-by-query';
 
-var SCROLL_TO_POSITION = 250;
+var SCROLL_TO_POSITION = 200;
 
 export default Ember.Controller.extend({
   queryParams: ['query', 'page'],
