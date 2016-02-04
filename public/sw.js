c4869959610b2a97cc9b4301f5470c2a7270fc83
Emmanuel Luciano <emma.luciano11@gmail.com>
2016-02-04 10:16:17 -0400

    Adds comments to methods.

--- public/sw.js
+++ public/sw.js
@@ -2,8 +2,6 @@ var addonsLink = 'https://io-builtwithember-addons-data.s3.amazonaws.com/addons.
 
 var CACHE_DEPENDENCIES_NAME = 'ADDONS_DEPENDENCIES';
 var CACHE_DEPENDENCIES = [
-  './',
-  'sw.js',
   'assets/vendor.css',
   'assets/vendor.js',
   'assets/ember-addons-website.css',
@@ -15,6 +13,8 @@ var CACHE_DEPENDENCIES = [
 ];
 CACHE_DEPENDENCIES.push(addonsLink);
 
+// Request the addons.js, filter the gravatar ulrs and
+// add the new urls to the CACHE_DEPENDENCIES array.
 function urlsToPrefetch() {
   return fetch(addonsLink).then(function(response) {
     return response.json();
@@ -29,9 +29,13 @@ function urlsToPrefetch() {
   });
 }
 
+// waitUntil expects a promise and it will stop the worker execution
+// until the promise is resolved.
 self.addEventListener('install', function(event) {
   event.waitUntil(
     urlsToPrefetch().then(function(urls) {
+      // Open the specified cache if it exists. If the cache doesn't exist
+      // create it and add prefetch the urls on the array.
       caches.open(CACHE_DEPENDENCIES_NAME).then(function(cache) {
         cache.addAll(urls);
       });
@@ -39,6 +43,8 @@ self.addEventListener('install', function(event) {
   );
 });
 
+// Intercept every request that the app makes. If it the request matches
+// a request that has been stored in the cache return the response that is stored.
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then(function(res) {
