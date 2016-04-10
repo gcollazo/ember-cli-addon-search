4d4b5a65475a28a02d80cdb47b1071bc7b758280
Emmanuel Luciano <emma.luciano11@gmail.com>
2016-04-09 20:07:31 -0400

    Modifies changes to custom logic in service worker.

--- app/serviceworkers/sw.js
+++ app/serviceworkers/sw.js
@@ -1,16 +1,16 @@
-const addonsLink = 'https://io-builtwithember-addons-data.s3.amazonaws.com/addons.json';
+const addonsUrl = 'https://io-builtwithember-addons-data.s3.amazonaws.com/addons.json';
 const GRAVATAR_CACHE = 'GRAVATAR_URLS';
 
 toolbox.router.get('/addons.json', toolbox.networkFirst, {origin: 'https://io-builtwithember-addons-data.s3.amazonaws.com'});
-toolbox.precache(addonsLink);
+toolbox.precache(addonsUrl);
 
 // Request the addons.js, filter the gravatar ulrs and return an array of gravatar urls.
 function gravatarUrls() {
-  return fetch(addonsLink).then(function(response) {
+  return fetch(addonsUrl).then(function(response) {
     return response.json();
   }).then(function(json) {
     return json.map(function(curr) {
-      return curr['_npmUser'].gravatar + '?s=30&d=retro';
+      return curr['_npmUser'].gravatar ? curr['_npmUser'].gravatar + '?s=30&d=retro':'';
     }).filter(function(data, index, array) {
       if (array.indexOf(data) === index) {
         return data;
@@ -20,45 +20,47 @@ function gravatarUrls() {
 }
 
 // Go through every gravatar url and remove the origin.
-// function addGravatarUrlsToRouter(urls) {
-//   const gravatarUrl = 'https://secure.gravatar.com';
-//   const regex = /https:\/\/secure.gravatar.com/g;
-
-//   urls.forEach(function(url) {
-//     const gravatarPath = url.replace(regex, '');
-
-//     toolbox.router.get(gravatarPath, toolbox.cacheFirst, {origin: gravatarUrl});
-//   });
-// }
+function isGravatarUrl(url) {
+  return url.indexOf('https://secure.gravatar.com');
+}
 
 // waitUntil expects a promise and it will stop the worker execution
 // until the promise is resolved.
 self.addEventListener('install', function(event) {
   event.waitUntil(
     gravatarUrls().then(function(urls) {
-      // addGravatarUrlsToRouter(urls);
-
       // Open the specified cache if it exists. If the cache doesn't exist
       // create it and add prefetch the urls on the array.
       caches.open(GRAVATAR_CACHE).then(function(cache) {
         cache.addAll(urls);
       });
-
-      if (self.skipWaiting) {
-        self.skipWaiting();
-      }
     })
   );
 });
 
-// Intercept every request that the app makes. If it the request matches
-// a request that has been stored in the cache return the response that is stored.
+/**
+  Intercept every request that the app makes. If it the request matches
+  a request that has been stored in the cache return the response that is stored.
+
+  If the request is not in the caches and if it's a new gravatar then fetch the new
+  gravatar and add it to the gravatars cache. After storing the response in the cache
+  we return a clone of the response to the app.
+*/
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(function(res) {
-      if (res) {
-        return res;
-      }
-    })
+    caches.match(event.request)
+      .then(function(res) {
+        if (res) {
+          return res;
+        } else if (isGravatarUrl(event.request.url) >= 0) {
+          return fetch(event.request.url).then(function(response) {
+            caches.open(GRAVATAR_CACHE).then(function(cache) {
+              cache.put(event.request, response);
+            });
+
+            return response.clone();
+          });
+        }
+      })
   );
 });
