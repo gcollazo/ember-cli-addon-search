0ab9b8019bdaf4cc24f6e8d7af51fa12c8b73eac
Giovanni Collazo <hello@gcollazo.com>
2016-06-16 10:47:34 -0400

    Fix failing test

--- tests/unit/helpers/split-prefix-test.js
+++ tests/unit/helpers/split-prefix-test.js
@@ -6,5 +6,5 @@ module('Unit | Helper | split prefix');
 test('it works', function(assert) {
   var result = splitPrefix(['ember-cli-testing']);
 
-  assert.equal(result.toString(), '<span class="name-prefix">ember-cli-</span> <span class="name-main">testing</span>');
+  assert.equal(result.toString(), '<span class="name-prefix">ember-cli-</span><span class="name-main">testing</span>');
 });
