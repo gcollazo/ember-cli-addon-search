c106730ca0d8dd1ff6b88bad277809b2587f9e12
Godfrey Chan <godfrey@tilde.io>
2016-10-11 17:14:42 -0700

    Make some build params configurable from ENV vars
    This makes the following build parameters configurable from ENV variables:
    
    * MINIFY_JS: minify the JS bundles (defaults to `true` in production, same as EmberCLI)
    * MINIFY_CSS: minify the CSS bundles (defaults to `true` in production, same as EmberCLI)
    * SOURCEMAPS: emit JS sourcemaps (defaults to `true` in development, same as EmberCLI)
    * GOOGLE_ANALYTICS: report metrics to Google analytics (defaults to `true` in production)
    * TWITTER_WIDGET: include the Twitter widget script (defaults to `true` in production)
    
    For example, when running benchmarks, you might want to do something
    like this to minimize external network requests:
    
    `GOOGLE_ANALYTICS=false TWITTER_WIDGET=false ember build -prod`
    
    See also https://github.com/emberobserver/client/commit/99230781b65585f15b85d7cc225ed45c8ee30442
    (part of https://github.com/emberobserver/client/pull/52).

--- app/app.js
+++ app/app.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
-import './ga';
 
 let App;
 
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,10 +1,34 @@
 /*jshint node:true*/
 /* global require, module */
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var parseFlag = require('./config/parse-flag');
+var env = EmberApp.env();
 
 module.exports = function(defaults) {
-  var app = new EmberApp(defaults, {
-  });
+  var options = {
+    inlineContent: {},
+    minifyJS: {},
+    minifyCSS: {},
+    sourcemaps: {
+      extensions: ['js']
+    }
+  };
+
+  if (parseFlag('TWITTER_WIDGET', env === 'production')) {
+    options.inlineContent['snippets/twitter-widget'] = 'app/snippets/twitter-widget.js';
+  }
+
+  if (parseFlag('GOOGLE_ANALYTICS', env === 'production')) {
+    options.inlineContent['snippets/google-analytics'] = 'app/snippets/google-analytics.js';
+  }
+
+  options.minifyJS.enabled = parseFlag('MINIFY_JS', env === 'production');
+
+  options.minifyCSS.enabled = parseFlag('MINIFY_CSS', env === 'production');
+
+  options.sourcemaps.enabled = parseFlag('SOURCEMAPS', env !== 'production');
+
+  var app = new EmberApp(defaults, options);
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
