09b6bb6c0e2a303b08309df21eb88934b0f08975
Josh Hollinshead <josh.hollinshead@alphero.com>
2017-03-14 14:02:08 +1300

    Remove mobile breakpoint check in liquid-fire transitions map to allow animations on mobile now that perf issue has been resolved

--- app/transitions.js
+++ app/transitions.js
@@ -1,5 +1,4 @@
 const opts = { duration: 300, delay: 100, easing: 'easeInOutQuart' };
-const media = '(min-width: 768px)';
 
 // Only animate between page results if the distance between the pages is one.
 // This avoids animations occuring when the distance between pages is possibly more 
@@ -8,7 +7,6 @@ const media = '(min-width: 768px)';
 export default function(){
   this.transition(
     this.hasClass('lf-slider'),
-    this.media(media),
     this.toValue(function(newPage, oldPage){
       return newPage.meta.page - 1 === oldPage.meta.page;
     }),
@@ -17,7 +15,6 @@ export default function(){
 
   this.transition(
     this.hasClass('lf-slider'),
-    this.media(media),
     this.toValue(function(newPage, oldPage){
       return newPage.meta.page + 1 === oldPage.meta.page;
     }),
