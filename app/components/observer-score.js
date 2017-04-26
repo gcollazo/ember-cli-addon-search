606b9ff04f1312b1c57e3be2c82afedc97fd217f
Giovanni Collazo <hello@gcollazo.com>
2017-04-26 08:10:52 -0400

    Update emberObserver score

--- app/components/observer-score.js
+++ app/components/observer-score.js
@@ -1,8 +1,19 @@
 import Ember from 'ember';
 
 const {
-  Component
+  Component,
+  computed
 } = Ember;
 
 export default Component.extend({
+  computedScore: computed('score', function() {
+    const score = this.get('score');
+    if (score === -2) {
+      return '-';
+    } else if (score === -1) {
+      return 'WIP';
+    } else {
+      return score;
+    }
+  })
 });
